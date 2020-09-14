58ab0cd2c1bfe962e3164f5f5ae63e76a956e320
joelmackenz <66138947+joelmackenz@users.noreply.github.com>
2020-09-13 19:38:30 -0700

    Add files via upload

---
+++ src/components/Dictionary/Dictionary.js
@@ -0,0 +1,30 @@
+let dictionary = {
+    //Lesson 1
+"每": [1, "měi", "each"],
+"次": [1, "cì", "second"],
+"干": [1, "gān", "to do"],
+"吗": [1, "má", "?"],
+"开": [1, "kāi", "open"],
+"恨": [1, "hěn", "very"],
+"谢": [1, "xiè", "thank"],
+    //Lesson 2
+"还": [2, "hái ", "still"],
+"能": [2, "néng", "can"],
+"块": [2, "kuài", "fast"],
+"慢": [2, "màn", "slow"],
+"语": [2, "yŭ", "language"],
+"言": [2, "yán", "words/talk"],
+"读": [2, "dú", "to read"],
+    //Lesson 3
+"虫": [3, "chóng", "insect"],
+"鸟": [3, "niăo", "bird"],
+"鱼": [3, "yú", "fish"],
+"牛": [3, "niú", "cow"],
+"羊": [3, "yáng", "sheep"],
+"肉": [3, "ròu", "meat"],
+"龙": [3, "lóng", "dragon"],
+"马": [3, "mă", "horse"],
+}
+
+
+export default dictionary;
