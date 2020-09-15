0e52519f0ede527280d1791d981b823ebbaf03d5
joelmackenz <66138947+joelmackenz@users.noreply.github.com>
2020-09-14 21:58:29 -0700

    Add files via upload
    Update on September 14 2020
    API search now up-and-running; sorts by popularity of characters.
    Some CSS changes.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Study from "./components/Study-page/Study-page";
 import DictionaryPage from "./components/Dictionary-page/Dictionary-page";
-import API from "./components/API/Mandarin-API";
+import API from "./components/API-page/Mandarin-API";
 import Nav from "./components/Nav/Nav";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import "./App.css";
@@ -13,11 +13,11 @@ class App extends React.Component {
       <Router> 
         <Nav/>
         <Switch>
-                <Route path="/" exact component={Study} />
-                <Route path="/study" component={Study} />
-                <Route path="/dictionary" component={DictionaryPage}/>
-                <Route path="/API" component={API}/>
-            </Switch>
+          <Route path="/" exact component={Study} />
+          <Route path="/study" component={Study} />
+          <Route path="/dictionary" component={DictionaryPage}/>
+          <Route path="/charactersearch" component={API}/>
+        </Switch>
       </Router>
     );
   }
