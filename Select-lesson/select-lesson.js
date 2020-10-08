235d0a5f7e11f57ea8dba479d2f31e4c065ffaf9
joelmackenz <66138947+joelmackenz@users.noreply.github.com>
2020-10-08 15:21:08 -0700

    Add files via upload
    Lesson display now completely renders dynamically according to what has been added to the dictionary.
    It will now be easier to add custom dictionary entries.

--- Select-lesson/select-lesson.js
+++ Select-lesson/select-lesson.js
@@ -13,12 +13,12 @@ export class selectLesson extends React.Component {
         this.props.onChange(name);
       }
 
-
       render() {
       let lessonCharList = [];
       let lessonCharEntry = [];
       let lesson = 3.1;
 
+      //Formula found on GitHub for rounding numbers to decimal places
       let precisionRound = (number, precision) => {
         var factor = Math.pow(10, precision);
         return Math.round(number * factor) / factor;
@@ -38,10 +38,10 @@ export class selectLesson extends React.Component {
           }
           lessonCharEntry = [];
           //lesson increase by .1
-          lesson = precisionRound((lesson + .1), 2);
+          lesson = precisionRound((lesson + .1), 3);
           while (lesson !== value[0]) {
             //while the current lesson number does not equal the next character's number, keep adding .1 until they are the same.
-            lesson = precisionRound((lesson + .1), 2)
+            lesson = precisionRound((lesson + .1), 3)
           }
           lessonCharEntry.push(key)
         }
@@ -53,73 +53,22 @@ export class selectLesson extends React.Component {
       //This creates an object which dynamically creates entries in an object to hold the lessons. They are titled according to
       //their first element's lesson number.
       let lessons = {}
-      let x = 0;
       for (const lesson of lessonCharList) {
         let lessonValue = dictionary[lesson[0]][0]
-        let lessonNum = 'lesson ' + JSON.stringify(lessonValue);
+        let lessonNum = 'Lesson ' + JSON.stringify(lessonValue);
         lessons[lessonNum] = lesson;
-        x++
       }
-      console.log(lessons)
-      //Next, I need to insert these values into the return().
-      //I need something that dynamically creates everything below "choose a lesson"
-
-
-
-      const bookThreeChars = [];
-      for (const [key, value] of Object.entries(dictionary)) {
-        if (Math.floor(value[0]) === 3) {
-          bookThreeChars.push(key)
-        }
-      }
-
-      const lessonOneChars = [];
-      for (const [key, value] of Object.entries(dictionary)) {
-        if (value.includes(3.1)) {
-          lessonOneChars.push(key)
-        }
-      }
-
-      const lessonTwoChars = [];
-      for (const [key, value] of Object.entries(dictionary)) {
-        if (value.includes(3.2)) {
-          lessonTwoChars.push(key)
-        }
-      }
-
-      const lessonThreeChars = [];
-      for (const [key, value] of Object.entries(dictionary)) {
-        if (value.includes(3.3)) {
-          lessonThreeChars.push(key)
-        }
-      }
-
-      const bookFourChars = [];
-      for (const [key, value] of Object.entries(dictionary)) {
-        if (Math.floor(value[0]) === 4) {
-          bookFourChars.push(key)
-        }
-      }
-
-      const b4L1Chars = [];
-      for (const [key, value] of Object.entries(dictionary)) {
-        if (value.includes(4.1)) {
-          b4L1Chars.push(key)
-        }
-      }
+     
 
         return (
           <div className="dropDown">
             <select
-              id="lessons"
+              id="lessonSelect"
               onChange={this.changeLesson}>
-              <option>Choose a lesson!</option>
-              <option value={bookThreeChars}>Book 3</option>
-              <option value={lessonOneChars}>Book 3, Lesson 1</option>
-              <option value={lessonTwoChars}>Book 3, Lesson 2</option>
-              <option value={lessonThreeChars}>Book 3, Lesson 3</option>
-              <option value={bookFourChars}>Book 4</option>
-              <option value={b4L1Chars}>Book 4, Lesson 1</option>
+              {Object.entries(lessons).map((entry) =>
+                <option value={entry[1]}>{entry[0]}</option>
+                )
+              }
             </select>
           </div>
         );
